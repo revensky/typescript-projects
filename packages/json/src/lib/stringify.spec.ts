b98b3eeb2b1c9a8533ead44e2f4f22cb3995d186
Revensky <eduardorbr7@gmail.com>
2025-11-09 21:13:25 -0300

    fix(@revensky/json): build not including js files

--- packages/json/src/lib/stringify.spec.ts
+++ packages/json/src/lib/stringify.spec.ts
@@ -2,6 +2,6 @@ import { stringify } from './stringify.js';
 
 describe('stringify()', () => {
   it('should stringify a javascript object into a json object as usual.', () => {
-    expect(stringify({ foo: 'bar', baz: 123 })).toStrictEqual('{"foo":"bar","baz":123}');
+    expect(stringify({ foo: 'bar', baz: 123 })).toEqual('{"foo":"bar","baz":123}');
   });
 });
